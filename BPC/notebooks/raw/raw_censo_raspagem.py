fa6130f20e96c8b91c1af6b4df7adb0bd4372312
Willdeglan de Sousa Santos <122640438+willdegl4n@users.noreply.github.com>
2025-08-26 12:20:16 -0300

    Update and rename raw_censo_ingestao.py to raw_censo_raspagem.py

--- BPC/notebooks/raw/raw_censo_raspagem.py
+++ BPC/notebooks/raw/raw_censo_raspagem.py
@@ -1,10 +1,8 @@
 # raw_censo_ingestao.py
 import requests
-import pandas as pd
-import os
 
 # Diretório de destino
-source_dir = "/Workspace/bpc/source"
+source_dir = "/Volumes/bpc/raw/source/"
 arquivo_destino = f"{source_dir}/censo_2022.csv"
 
 # URL da API SIDRA para população do Censo 2022
@@ -22,20 +20,4 @@ try:
     print(f"✅ Arquivo salvo com sucesso em: {arquivo_destino}")
 except Exception as e:
     print(f"❌ Erro ao baixar arquivo: {e}")
-    raise e
-
-#========================================
-# raw_censo_ingestao.py (ler o arquivo)
-#========================================
-
-# Caminho do arquivo no workspace
-source_path = "/Workspace/bpc/source/censo_2022.csv"
-
-try:
-    print(f"Lendo arquivo Censo 2022 de: {source_path}")
-    # Lê o arquivo CSV com pandas
-    df = pd.read_csv(source_path)
-    print(f"✅ Arquivo lido com sucesso. Primeiras linhas:\n{df.head()}")
-except Exception as e:
-    print(f"❌ Erro ao ler arquivo: {e}")
     raise e
