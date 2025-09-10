8123d4a05cb287125338ca7ecf6e18fa11342bb5
willdeglan@gmail.com <willdeglan@gmail.com>
2025-09-10 14:58:58 +0000

    update in databricks

--- Pokemon/pokemon_extract.py
+++ Pokemon/pokemon_extract.py
@@ -1,11 +1,11 @@
-#importando bibliotecas
+# importando bibliotecas
 import boto3  # para conexão com aws
 import json   # para organizar ler e salvar json 
 import requests  #para uso de solicitações da API
 
 # função recuperar dados da api
 def extract_all_pokemon():
-  base_url = "http://pokeapi.com/api/v2/pokemon/"
+  base_url = "https://pokeapi.co/api/v2/pokemon/"
   pokemon_list = []
   next_url = base_url
 
@@ -20,3 +20,12 @@ def extract_all_pokemon():
   
   return pokemon_list
 
+# recebenco os valores da função e gravando em um dataframe
+pokemons = spark.createDataFrame(extract_all_pokemon())
+
+# criando a estrutura e salvando o dataframe em uma table
+spark.sql("CREATE CATALOG IF NOT EXISTS pokemon")
+spark.sql("CREATE SCHEMA IF NOT EXISTS pokemon.bronze")
+
+# salvando no bronze
+pokemons.write.mode("overwrite").saveAsTable("pokemon.bronze.api")
